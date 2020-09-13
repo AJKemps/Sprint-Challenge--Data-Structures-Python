2098e723ab0c501fa5e218cf6bb72a15d34502ef
Alex Kemper <alex.j.kemper@gmail.com>
2020-09-13 17:31:37 -0400

    done with mvp

--- reverse/reverse.py
+++ reverse/reverse.py
@@ -12,6 +12,7 @@ class Node:
     def set_next(self, new_next):
         self.next_node = new_next
 
+
 class LinkedList:
     def __init__(self):
         self.head = None
@@ -39,4 +40,14 @@ class LinkedList:
         return False
 
     def reverse_list(self, node, prev):
-        pass
+
+        prev = None
+        current = self.head
+        while current:
+            nxt = current.next_node
+            current.next_node = prev
+
+            prev = current
+            current = nxt
+
+        self.head = prev
