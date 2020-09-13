bcb597a363a22b16617dc6aeda7ade9113b344d2
Alex Kemper <alex.j.kemper@gmail.com>
2020-09-13 14:24:35 -0400

    pushing progress in time for submission deadline but nowhere near finished...still working on names and reverse, only the first problem is done

--- names/names.py
+++ names/names.py
@@ -13,14 +13,67 @@ f.close()
 duplicates = []  # Return the list of duplicates in this data structure
 
 # Replace the nested for loops below with your improvements
-for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+
+sorted_list1 = [name for name in names_1]
+sorted_list2 = [name for name in names_2]
+sorted_list3 = sorted_list1 + sorted_list2
+sorted_list3.sort()
+
+
+print(len(sorted_list1))
+print(len(sorted_list2))
+print(len(sorted_list3))
+print(sorted_list3[0], sorted_list3[1], sorted_list3[2])
+print(sorted_list3[-3], sorted_list3[-2], sorted_list3[-1])
+
+
+class BSTNode:
+    def __init__(self, value, left=None, right=None):
+        self.value = value
+        self.left = left
+        self.right = right
+
+    def insert(self, value):
+        if self.value is None:
+            self.value = BSTNode(value)
+        elif value >= self.value:
+            if self.right:
+                return self.right.insert(value)
+            else:
+                self.right = BSTNode(value)
+        elif value < self.value:
+            if self.left:
+                return self.left.insert(value)
+            else:
+                self.left = BSTNode(value)
+
+    def contains(self, target):
+
+        if target == self.value:
+            return True
+        elif target > self.value and self.right:
+            return self.right.contains(target)
+        elif target < self.value and self.left:
+            return self.left.contains(target)
+        else:
+            return False
+
+
+midpoint = int(len(sorted_list3) / 2)
+
+BST = BSTNode(midpoint)
+
+for name in sorted_list3:
+    BST.insert(name)
+
+for name in sorted_list3:
+    BST.contains(name)
+
 
 end_time = time.time()
-print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
-print (f"runtime: {end_time - start_time} seconds")
+
+print(f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
+print(f"runtime: {end_time - start_time} seconds")
 
 # ---------- Stretch Goal -----------
 # Python has built-in tools that allow for a very efficient approach to this problem
